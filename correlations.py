fed225fff72849f12ad91b4edec76ff4acc95b89
djanloo <zzibecu@gmail.com>
2022-09-28 00:18:45 +0200

    Code for correlations and figure

--- correlations.py
+++ correlations.py
@@ -10,6 +10,8 @@ rcParams["font.size"] = 11
 
 # Gets the data
 data = pd.read_csv("ravdess_features.csv")
+data.sex = (-2*pd.Categorical( data.sex).codes + 1)
+print(data.sex)
 qualitative = [
     "modality",
     "vocal_channel",
@@ -18,20 +20,24 @@ qualitative = [
     "statement",
     "repetition",
     "actor",
-    "sex",
     "channels",
     "sample_width",
     "frame_rate",
     "frame_width",
+    "stft_max",
+    "frame_count",
+    "length_ms",
+    "mean",
+    "max","min",
+    "kur","skew",
 ]
 labels = list(data.keys())
-
 
 for p in labels:
     if p in qualitative:
         data=data.drop(columns=[p])
-
-label_to_index = dict(zip(data.keys(), range(len(data))))
+labels = list(data.keys())
+label_to_index = dict(zip(labels, range(len(data))))
 data = data.to_numpy()
 # Fill I values
 def I(std):
@@ -41,5 +47,10 @@ fill_index = np.where(np.isnan(data[:,label_to_index["intensity"]]))
 fill_std = data[:,label_to_index["std"]][fill_index]
 data[:,label_to_index["intensity"]][fill_index] = I(fill_std)
 
-plt.imshow(np.corrcoef(data.T))
+plt.figure(figsize=(8,8))
+plt.imshow(np.corrcoef(data.T),cmap='RdBu')
+plt.xticks(range(len(labels)), labels=labels, rotation=90)
+plt.yticks(range(len(labels)), labels=labels, rotation=0)
+
+plt.colorbar()
 plt.show()
