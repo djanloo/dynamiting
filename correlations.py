9893ac07b615d73f38bb27548a62c48c9c52a600
djanloo <zzibecu@gmail.com>
2022-09-28 11:00:15 +0200

    corrected use of pandas' dataframes

--- correlations.py
+++ correlations.py
@@ -11,10 +11,11 @@ rcParams["font.size"] = 11
 
 # Gets the data
 data = pd.read_csv("ravdess_features.csv")
-data.sex = (-2*pd.Categorical( data.sex).codes + 1)
+
+# Converts to categorical
+data.sex = (-2*pd.Categorical(data.sex).codes + 1) # +- 1 for simplicity
 data.emotion = pd.Categorical(data.emotion).codes
 
-print(data.sex)
 excluded = [
     "modality",
     "vocal_channel",
@@ -33,49 +34,37 @@ excluded = [
     "max","min",
     # "kur","skew",
 ]
-labels = list(data.keys())
-
 data = data.drop(columns=excluded)
 
-labels = list(data.keys())
-label_to_index = dict(zip(labels, range(len(data))))
-
-data = data.to_numpy()
-# Fill I values
+### Fills missing values
 def I(std):
+    # Values obtained by log-linear regression
     return 20*np.log10(std/0.993)
 
-fill_index = np.where(np.isnan(data[:,label_to_index["intensity"]]))
-fill_std = data[:,label_to_index["std"]][fill_index]
-data[:,label_to_index["intensity"]][fill_index] = I(fill_std)
+i = data.intensity.to_numpy()
+std = data['std'].to_numpy()
+# Computes only the missing values
+i[np.isnan(i)] = I(std[np.isnan(i)])
+# Refills the dataframe
+data.intensity = i
 
+# Correlation plot
 plt.figure(figsize=(8,8))
-plt.imshow(np.corrcoef(data.T),cmap='RdBu')
-plt.xticks(range(len(labels)), labels=labels, rotation=90)
-plt.yticks(range(len(labels)), labels=labels, rotation=0)
-
+plt.imshow(np.corrcoef(data.T), cmap='RdBu')
+plt.xticks(range(len(data.keys())), labels=data.keys(), rotation=90)
+plt.yticks(range(len(data.keys())), labels=data.keys(), rotation=0)
 plt.colorbar()
 
+
+# Scatter plot
 plt.figure(figsize=(8,8))
-data = data.T
 
-male_mfcc_max = data[label_to_index['mfcc_max']][np.where(data[label_to_index["sex"]]==-1)]
-female_mfcc_max = data[label_to_index['mfcc_max']][np.where(data[label_to_index["sex"]]==1)]
-male_stft_mean = data[label_to_index['stft_mean']][np.where(data[label_to_index["sex"]]==-1)]
-female_stft_mean = data[label_to_index['stft_mean']][np.where(data[label_to_index["sex"]]==1)]
-male_emotions = data[label_to_index['emotion']][np.where(data[label_to_index["sex"]]==-1)]
-female_emotions = data[label_to_index['emotion']][np.where(data[label_to_index["sex"]]==1)]
-
-plt.scatter(male_mfcc_max, male_stft_mean, 
-                            marker="+", 
-                            label='male',
-                            c=male_emotions)
-plt.scatter(female_mfcc_max, female_stft_mean, 
-                                marker="^", 
-                                label='female',
-                                c=female_emotions)
+for sex, label in zip([-1,1], ['male', 'female']):
+    plt.scatter(data.mfcc_max[data.sex==sex], data.stft_mean[data.sex==sex], 
+                                marker="+", 
+                                label=label,
+                                c=data.emotion[data.sex==sex])
 
 plt.colorbar()
-
 plt.legend()
 plt.show()
